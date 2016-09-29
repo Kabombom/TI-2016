88d1b6c0ae1d6f9d9db08b9d18c3c7a660da6f5c
teresalazar13 <teresa.sal13@gmail.com>
2016-09-29 16:35:04 +0100

    main and exercise 1.

---
+++ histogramaOcurrencias.m
@@ -0,0 +1,4 @@
+function histogramaOcurrencias(alfabeto)
+    
+end
+
---
+++ main.m
@@ -0,0 +1,7 @@
+warning off
+
+clear, clc, close all
+
+% exercicio 1
+alfabeto = [1,2,3,4,5,6,7];
+histogramaOcurrencias(alfabeto);

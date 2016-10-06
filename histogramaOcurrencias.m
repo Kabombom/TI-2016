93d76103e84f3310fe39afa8a91a983b1c5639ec
Gonçalo Amaral <goa.docs@gmail.com>
2016-10-06 02:51:39 +0100

    Funçao histogramaOcurrencias mais geral. Adiçao de alfabetos e fontes para teste ao ficheiro main

--- histogramaOcurrencias.m
+++ histogramaOcurrencias.m
@@ -1,8 +1,32 @@
 function histogramaOcurrencias(p, A)
-    hist = zeros(length(A), 1);
-    for i = 1:length(A)
-        hist(i) = length(find(p == i));
+    %Verificar se é uma string
+    
+    [lines, cols] = size(p);
+    
+    %Se for um som
+    if(lines > 1) 
+        p = vertcat(p(:,1),p(:,2));
+        p = transpose(p);
     end
-    bar(hist);
-    set(gca,'ytick',0:10)
+    
+    %Se for alfabeto
+    if(ischar(p))
+        %converter letras para inteiros
+        p = uint16(p);
+        
+        %converter alfabeto para inteiros
+        convA = cellfun(@uint16, A);  
+    %Se for matrix de numeros    
+    else
+        %converte celulas em numeros
+        convA = cell2mat(A);
+        %converte numeros em strings
+        A = cellfun(@num2str, A, 'UniformOutput', false);       
+    end
+           
+    %Associa alfabeto dado aos itens na matriz, sendo apenas apresentados
+    %elementos do alfabeto
+    graf = categorical(p,convA,A);
+    histogram(graf);
+    
 end
--- main.m
+++ main.m
@@ -3,6 +3,9 @@ warning off
 clear, clc, close all
 
 % exercicio 1
-p = [1,2,4,2,5,7,10,9,7,1,1];
-A = 1:10;
-histogramaOcurrencias(p, A);
+som = [1 2; 3 4; 1 2; 3 4; 1 9];
+num = [1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 ];
+str = 'aeiouaeioaeiaeakas';
+alfStr = {'a' 'e' 'i' 'o' 'u'};
+alfNumSom = {1 2 3 4 5 6 7 8 9 10};
+histogramaOcurrencias(str, alfStr);

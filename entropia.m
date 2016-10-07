f39e66ff5180c2561fa56f83df334e134221271e
teresalazar13 <teresa.sal13@gmail.com>
2016-10-08 00:04:12 +0100

    exercise 2 - entropy

--- entropia.m
+++ entropia.m
@@ -1,10 +1,12 @@
-function entropia(p, A)
-  %somatorio da probabilidade de ocorrer cada simbolo de A em p
-  %exemplo: A= {S0 S1 S2 S3} com pk = probabilidade de Sk ocorrer em p
-  %exemplo p0 = 0.5, p1=0.25, p2=0.125, p3=0.125
-  %entropia =  0.5 * log2(1/0.5) + 0.25 * log2(1/0.25) + 0.125 * log2(1/0.125) + 0.125 * log2(1/0.125) = 1.75
-  %FOR HELP: http://www.ebah.pt/content/ABAAAfeXAAL/codificacao-fonte-dados
-  %WARNINGS: nao esquecer que o alfabeto introduzido nao é um array normal mas sim um array de celulas
-  %          -> Exemplo: array normal: [1 2 3] array de celulas: {1 2 3}
-  %          -> Ver implementaçao do exercicio 1
+function ent = entropia(p, A)
+  % exemplo: A= {S0 S1 S2 S3} com pk = probabilidade de Sk ocorrer em p
+  % exemplo p0 = 0.5, p1=0.25, p2=0.125, p3=0.125
+  % entropia =  0.5 * log2(1/0.5) + 0.25 * log2(1/0.25) + 0.125 * log2(1/0.125) + 0.125 * log2(1/0.125) = 1.75
+  hist = zeros(1, length(A));
+  for i = 1:length(A)
+    hist(i) = length(find(p == i)) / length(A);
+    end
+  ent = -sum(hist.*log2(hist));
+  s = sprintf('entropia %d\n', ent);
+  disp(s);
 end
--- main.m
+++ main.m
@@ -9,3 +9,8 @@ str = 'aeiouaeioaeiaeakas';
 alfStr = {'a' 'e' 'i' 'o' 'u'};
 alfNumSom = {1 2 3 4 5 6 7 8 9 10};
 histogramaOcurrencias(str, alfStr);
+
+% exercicio 2
+a = [1,2,3,1,2];
+aAlf = [1,2,3];
+entropia(a, aAlf);

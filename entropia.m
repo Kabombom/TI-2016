f0c71a2b6d38039f5fa04fe79985aad02176a51f
Gonçalo Amaral <goa.docs@gmail.com>
2016-10-07 18:50:04 +0100

    Added description of how to do exercise 2

---
+++ entropia.m
@@ -0,0 +1,7 @@
+function entropia(p, A)
+  %somatorio da probabilidade de ocorrer cada simbolo de A em p
+  %exemplo: A= {S0 S1 S2 S3} com pk = probabilidade de Sk ocorrer em p
+  %exemplo p0 = 0.5, p1=0.25, p2=0.125, p3=0.125
+  %entropia =  0.5 * log2(1/0.5) + 0.25 * log2(1/0.25) + 0.125 * log2(1/0.125) + 0.125 * log2(1/0.125) = 1.75
+  %FOR HELP: http://www.ebah.pt/content/ABAAAfeXAAL/codificacao-fonte-dados
+end

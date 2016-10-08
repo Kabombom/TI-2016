9042e9c804a55f6c3695f336ebe72c42755707e1
Gonçalo Amaral <goa.docs@gmail.com>
2016-10-08 18:58:22 +0100

    Changed exercise 2 to full vector functional approach

--- entropia.m
+++ entropia.m
@@ -1,12 +1,23 @@
-function ent = entropia(p, A)
+function ret = entropia(p, A)
   % exemplo: A= {S0 S1 S2 S3} com pk = probabilidade de Sk ocorrer em p
-  % exemplo p0 = 0.5, p1=0.25, p2=0.125, p3=0.125
-  % entropia =  0.5 * log2(1/0.5) + 0.25 * log2(1/0.25) + 0.125 * log2(1/0.125) + 0.125 * log2(1/0.125) = 1.75
-  hist = zeros(1, length(A));
-  for i = 1:length(A)
-    hist(i) = length(find(p == i)) / length(A);
-    end
-  ent = -sum(hist.*log2(hist));
+  % exemplo p0=0.5, p1=0.25, p2=0.125, p3=0.125
+  % entropia = 0.5 * log2(1/0.5) + 0.25 * log2(1/0.25) + 0.125 * log2(1/0.125) + 0.125 * log2(1/0.125) = 1.75  
+  %Obtenho o objecto do tipo categorical
+  graf = histogramaOcurrencias(p, A);
+  %obter frequencias
+  counts = histcounts(graf);
+  frequencias = counts(1,:);
+  %numero de elementos em p
+  numElementos = length(p);
+  %calculo a probabilidade de cada elemento de ocorrer
+  probs = arrayfun(@(x) x./numElementos, frequencias);
+  %Uso a formula mas sem fazer somatorio
+  preSum = arrayfun(@(x) x .* log2(1./x), probs);
+  %Torno todos os elementos do tipo Nan em 0, se nao o fizer a sum vai dar Nan
+  preSum(isnan(preSum)) = 0;
+  %Somatorio para calcular a entropia
+  ent = sum(preSum);
+  %Classico print
   s = sprintf('entropia %d\n', ent);
   disp(s);
 end

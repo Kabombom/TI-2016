192de19dc471a6a5460fe9183737a89ee62c80d7
teresalazar13 <teresa.sal13@gmail.com>
2016-10-12 17:48:33 +0100

    exercise 4 - Huffman entropy

---
+++ EntropiaHufflen.m
@@ -0,0 +1,72 @@
+function EntropiaHufflen()
+
+    % Lena.bmp
+    imagem = imread('data/Lena.bmp');
+    imagem = imagem(:);
+    c = hist(imagem, 0:255);
+    huf = hufflen(c);
+    ent = sum((c/sum(c)) .* huf);
+    s = sprintf('entropia de Lena.bmp %d\n', ent);
+    disp(s);
+    
+    % CT1.bmp
+    imagem = imread('data/CT1.bmp');
+    imagem = imagem(:);
+    c = hist(imagem, 0:255);
+    huf = hufflen(c);
+    ent = sum((c/sum(c)) .* huf);
+    s = sprintf('entropia de CT1.bmp %d\n', ent);
+    disp(s);
+    
+    % Binaria.bmp
+    imagem = imread('data/Binaria.bmp');
+    imagem = imagem(:);
+    c = hist(imagem, 0:255);
+    huf = hufflen(c);
+    ent = sum((c/sum(c)) .* huf);
+    s = sprintf('entropia de Binaria.bmp %d\n', ent);
+    disp(s);
+    
+    % saxriff.wav
+    [som, freq, nBits ] = getSoundData('data/saxriff.wav');
+    d = 1/(2^nBits);
+    alf = num2cell(-1:d:1);
+    [lines, collumns] = size(som);
+    if(lines > 1) 
+        %converter matrix numa linha apenas
+        som = reshape(som, [1, lines*collumns]);
+        %converte celulas em numeros
+        convA = cell2mat(alf);
+
+        graf = zeros(1,lines*collumns); 
+        counter = 1;
+        maxConvA = length(convA);
+        maxP = length(som);
+        for i=1:maxConvA
+            for j=1:maxP
+                if(convA(i)==som(j))
+                   graf(counter) = som(j);
+                   counter = counter + 1;
+                end
+            end
+        end
+    end
+    freqOcurr = hist(graf)
+    HLen = hufflen(freqOcurr);
+    ent = sum((freqOcurr/sum(freqOcurr)) .* HLen);
+    s = sprintf('entropia de saxriff.wav %d\n', ent);
+    disp(s)
+    
+    % Texto.txt
+    letters = {'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'};
+    ficheiro = fopen('data/Texto.txt');
+    texto = fscanf(ficheiro, '%c');
+    fclose(ficheiro);
+    graf = histogramaOcurrencias(texto , letters);
+    freqOcurr = hist(graf)
+    HLen = hufflen(freqOcurr);
+    ent = sum((freqOcurr/sum(freqOcurr)) .* HLen);
+    s = sprintf('entropia de Texto.txt %d\n', ent);
+    disp(s)
+    
+end
--- main.m
+++ main.m
@@ -2,10 +2,27 @@ warning off
 
 clear, clc, close all
 
+% exercicio 1
+imagem = imread('data/CT1.bmp');
+som = [1 2; 3 4; 1 2; 3 4; 1 9];
+num = [1 1 1 1 1 1 1 1 2 2 2 2 3 3 4 4];
+str = 'aeiouaeioaeiaeakas';
+alfStr = {'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'};
+alfNumSom = {1 2 3 4 5 6 7 8 9 10};
+%displayHistograma(imagem);
+
+
+%exercicio 2
+%entropia(num, alfNumSom); ->ver como chamar funcao
+
 %exercicio 3
 % distribuicaoEstatistica_Entropia('data/saxriff.wav');
 % distribuicaoEstatistica_Entropia('data/Lena.bmp');
 % distribuicaoEstatistica_Entropia('data/CT1.bmp');
 % distribuicaoEstatistica_Entropia('data/Binaria.bmp');
 % distribuicaoEstatistica_Entropia('data/Texto.txt');
+
+%exercicio 4
+EntropiaHufflen();
+
 

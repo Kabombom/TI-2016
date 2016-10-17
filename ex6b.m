e62d6804106173ebecfba21bab4172876dc25827
teresalazar13 <teresa.sal13@gmail.com>
2016-10-17 13:17:57 +0100

    exercise 6b

--- ex6b.m
+++ ex6b.m
@@ -5,11 +5,18 @@
 
 som = som(:);
 somTarget01 = somTarget01(:);
+somTarget02 = somTarget02(:);
 som = som * 2^nBits;
 somTarget01 = somTarget01 * 2^nBitsTarget01;
+somTarget02 = somTarget02 * 2^nBitsTarget02;
 d = 1/(2^nBits);
 alf = num2cell(-1:d:1);
-
 step = floor(1/4 * length(som));
+
 info = informacaoMutua(som, somTarget01, alf, step);
 disp(info);
+plot(info);
+
+info2 = informacaoMutua(som, somTarget02, alf, step);
+disp(info2);
+plot(info2);

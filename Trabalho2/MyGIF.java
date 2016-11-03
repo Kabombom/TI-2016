ac26aeda82bdd0b0642bf2549a998daf634c7100
Kabombom <mantonio@student.dei.uc.pt>
2016-11-03 16:57:31 +0000

    Changes code style

--- Trabalho2/MyGIF.java
+++ Trabalho2/MyGIF.java
@@ -2,27 +2,24 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import java.io.*;
 
-//classe para ler uma imagem e preparar a codificação no formato GIF
-public class MyGIF  
-{
+// Classe para ler uma imagem e preparar a codificacao no formato GIF
+public class MyGIF {
 	static short width, height;
 	static byte [] r, g, b;
 	static byte pixels[], colors[];
 	
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		/*if (args.length != 2) 
 		{
 			System.out.println("MyGIF <input file> <output file>");
 			return;
 		}*/
 				
-		try
-		{		
+		try {
 			//---Carregar uma imagem
 			String imName = "monica.png"; //args[0];
 			Image image = ImageIO.read(new File(imName));
-			
+
 			//---Codificar a imagem como GIF		
 			MyGIFEncoder encoder = new MyGIFEncoder(image);
 		
@@ -31,22 +28,19 @@ public class MyGIF
 			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(gifName));		
 			
 			////////////////////////////////////////////////////////
-			encoder.write(output);			//---> COMPLETAR ESTA FUNÇÃO
+			encoder.write(output);			//---> COMPLETAR ESTA FUNCAO
 			////////////////////////////////////////////////////////				
 		}
-		catch(IOException e)
-		{
+		catch(IOException e) {
 			System.out.println("Erro no acesso ao ficheiro (input)");
 			System.exit(-1);
 		}
-		catch(InterruptedException e)
-		{
+		catch(InterruptedException e) {
 			System.out.println("Erro ao realizar o 'grabbing' dos pixeis");
 			System.exit(-1);
 		}
-		catch(AWTException e)
-		{
-			System.out.println("Erro no acesso à imagem");
+		catch(AWTException e) {
+			System.out.println("Erro no acesso a imagem");
 			System.exit(-1);
 		}				
 	}	
